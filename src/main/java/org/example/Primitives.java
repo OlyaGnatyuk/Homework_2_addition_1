c8ecb7eec5527a51e4e9fe4c6f23f60a9a6ef2a9
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-26 22:25:31 +0300

    Initial commit

---
+++ src/main/java/org/example/Primitives.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Primitives {
+    byte a = 5;
+    short b = 8;
+    int c = 10;
+    long d = 13L;
+    double doubleNumber = 16.78;
+    float floatNumber = 75.8f;
+    boolean isLearning = true;
+    char symbol = 'k';
+}
---
+++ src/main/java/org/example/Wrappers.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Wrappers {
+    Byte a = 5;
+    Short b = 8;
+    Integer c = 10;
+    Long d = 15L;
+    Double doubleNumber = 16.78;
+    Float floatNumber = 75.8f;
+    Boolean isLearning = true;
+    Character symbol = 'k';
+}
